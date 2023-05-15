9f37dd568b4f9f23077d66c5138f5f3f10a12d6f
daugiss <luksasdaugardas@gmail.com>
2023-05-15 20:10:18 +0300

    Add scope functioanlity to SymbolsTable

--- src/main/java/org/Functive/FunctiveSymbolsTable.java
+++ src/main/java/org/Functive/FunctiveSymbolsTable.java
@@ -5,27 +5,34 @@ import java.util.Map;
 import java.util.Stack;
 
 public class FunctiveSymbolsTable {
-    Stack<Map<String,Object>> stack;
-    public Map<String,Object> currentTable;
-    public FunctiveSymbolsTable(){
+    Stack<Map<String, Object>> stack;
+    public Map<String, Object> currentTable;
+
+    public FunctiveSymbolsTable() {
         stack = new Stack<>();
         currentTable = new HashMap<>();
     }
 
-    public void enterBlock(boolean passVars) {
+    public void enterBlock() {
         stack.push(currentTable);
-        if(!passVars){
-            currentTable = new HashMap<>();
+        Map<String, Object> scopedTable = new HashMap<>();
+        // add outside variables to the currentTable
+        for (Map.Entry<String, Object> entry : currentTable.entrySet()) {
+            scopedTable.put(entry.getKey(), entry.getValue());
         }
-        else{
-            currentTable = new HashMap<>(currentTable);
-        }
+        currentTable = scopedTable;
     }
-    public void exitBlock(boolean applyChanges) {
-        Map<String,Object> last = stack.pop();
-        if(applyChanges) {
-            last.replaceAll((k, v) -> currentTable.get(k));
+
+    public void exitBlock() {
+        Map<String, Object> exitedScopedTable = currentTable;
+        currentTable = stack.pop();
+
+        // add the variables which have the same names as the outside variables to the
+        // currentTable
+        for (Map.Entry<String, Object> entry : exitedScopedTable.entrySet()) {
+            if (currentTable.containsKey(entry.getKey())) {
+                currentTable.put(entry.getKey(), entry.getValue());
+            }
         }
-        currentTable = last;
     }
 }
